912960f39a7930a9cffd7136ce5927f0fca4bdd0
mike prior <mprior19xx@gmail.com>
2020-01-14 15:28:25 -0500

    added new files in

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-const myVM = (() => {
-    // Instantiate a new Vue instance
-    let vue_vm = new Vue({
-        data: {
-            message: "Hello from Vue!",
-            
-            collection: [
-                { name: "Trevvy", role: "Unknown" },
-                { name: "Joe", role: "Picking Up The Slack" },
-                { name: "Justin", role: "Giving Brisk 0s" }
-            ]
-        },
+var vm = new Vue({
+  el: "#app",
 
-        methods: {
-            logClicked() {
-                console.log('Clicked on an element!');
-            }
-        }
-    }).$mount("#app");
-})();
+  data: {
+
+    // mock up the user - this well eventually come from the database UMS (user management system)
+
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+
+  }
+});
